e772a10dc2343a83397a2875afdb8c7db8769644
Ondřej Váně <onry.vane@gmail.com>
2020-11-18 11:53:29 +0100

    Initialization file for anti-pattern  busioness as usual

---
+++ src/business_as_usual.sql
@@ -0,0 +1,18 @@
+/*
+Anti-pattern name: Business as usual (No sprint retrospective)
+
+Description: Absence of a retrospective after individual
+             iterations or after the completion project
+
+Detection: There will be no activities in the project
+           that would indicate that a retrospective is
+           taking place (issue with the name of the
+           retrospective, issue on which all team members
+           log, issue that is repeated periodically,
+           issue to which no commit is bound, issue which
+           will be marked as administration or something like that).
+           There will be no notes in the wiki or other tool called
+           retrospectives (% retro%).
+*/
+
+SELECT * FROM `workunitview`
