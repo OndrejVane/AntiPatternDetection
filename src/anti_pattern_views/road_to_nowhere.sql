8d87c1a21ff9daf51e9e7e2e86bad8064e05557e
Ondřej Váně <onry.vane@gmail.com>
2021-01-13 16:16:12 +0100

    Road To Nowhere
    + create new version of view
    + look for project plan in wiki

--- src/anti_pattern_views/road_to_nowhere.sql
+++ src/anti_pattern_views/road_to_nowhere.sql
@@ -7,9 +7,11 @@ Description: The project is not sufficiently planned and therefore
 
 Detection: There is no activity in ALM that would indicate the creation
            of a project plan. There will be no document in the wiki
-           called the "Project Plan".
+           called the "Project Plan". Project plan should be created in first or
+           second iteration.
 
-           TODO: hledat projektový plán v dms nebo ve wiki => zeptat se, kde jsou uloženy dokumenty z wiki nebo DMS
+           TODO: 1) detekovat, že na začátku iterací probíhá plánování
+
 */
 
 create or replace view road_to_nowhere_view as
@@ -37,3 +39,26 @@ create or replace view road_to_nowhere_view as
     order by project.id;
 
 select * from road_to_nowhere;
+
+select * from fieldchangeview;
+
+/* NOVÁ VERZE */
+set @projectId = 5;
+set @firstIterationStartDate = (select startDate from iteration where superProjectId = @projectId ORDER BY startDate LIMIT 1 offset 0);
+set @secondIterationStartDate = (select startDate from iteration where superProjectId = @projectId ORDER BY startDate LIMIT 1 offset 1);
+set @numberOfIssuesForProjectPlan = (SELECT count(*) from workunitview where projectId = @projectId  and (
+    				workunitview.name like '%plán%projektu%' or
+                    workunitview.name like '%project%plan%' or
+                    workunitview.name like '%plan%project%' or
+                    workunitview.name like '%proje%plán%')
+					AND
+					(iterationStartDate = @firstIterationStartDate OR
+                        iterationStartDate = @secondIterationStartDate));
+set @numberOfWikipagesForProjectPlan = (SELECT count(*) from artifactview where projectId = @projectId AND artifactClass like 'WIKIPAGE' AND (
+    				artifactview.name like '%plán%projektu%' or
+                    artifactview.name like '%project%plan%' or
+                    artifactview.name like '%plan%project%' or
+                    artifactview.name like '%proje%plán%'));
+
+select @projectId, @numberOfIssuesForProjectPlan, @numberOfWikipagesForProjectPlan;
+
