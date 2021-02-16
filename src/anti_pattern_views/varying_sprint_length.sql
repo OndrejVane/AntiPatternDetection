009761d3bac171c7bd7b7e7d73fd21414ab9ceac
Ondřej Váně <onry.vane@gmail.com>
2021-02-16 18:22:00 +0100

    Varying sprint length
    * refactor code

--- src/anti_pattern_views/varying_sprint_length.sql
+++ src/anti_pattern_views/varying_sprint_length.sql
@@ -15,25 +15,25 @@ Detection: Detect sprint lengths throughout the project
            Too Long sprint anti-pattern.
 */
 
-create or replace view varying_sprint_length_view as
-    select project.id as `ID`,
-           project.name as `Project Name`,
-           project.description as `Description`,
-           count(*) as `Number Of Iterations Without Extremes`,
-           std(datediff(iteration.endDate, iteration.startDate)) as `Standard Deviation Of Iteration Length - σ`,
-           std(datediff(iteration.endDate, iteration.startDate)) * std(datediff(iteration.endDate, iteration.startDate)) as `Deviation Of Iteration Length - σ^2`,
-           if(
-                std(datediff(iteration.endDate, iteration.startDate))
-                >
-                (SELECT value FROM anti_pattern_properties a where a.key = 'max_standard_deviation_for_iteration_length'), true, false) as `Anti-pattern occurrence`
-    from project inner join iteration on project.id = iteration.superProjectId
-    where project.id = iteration.superProjectId
-          and
-            iteration.id != (select id from iteration where superProjectId = project.id order by startDate limit 1 )
-          and
-            iteration.id != (select id from iteration where superProjectId = project.id order by startDate desc limit 1 )
-    group by project.id
-    order by project.id;
-
-select * from varying_sprint_length_view;
+/* Init project id */
+set @projectId = 5;
+/* Maximum sprint length variance */
+set @maxSprintVariance = 1;
+/* Exclude first and last iteration? */
+set @excludeFirstAndLastIteration = true;
+/* Id of first iteration */
+set @idOfFirstIteration = (select id from iteration where iteration.superProjectId = @projectId order by startDate limit 1);
+/* Id of last iteration */
+set @idOfLastIteration = (select id from iteration where iteration.superProjectId = @projectId order by startDate desc limit 1);
+/* Select all too long iterations */
+select std(datediff(iteration.endDate, iteration.startDate)) as `Standard Deviation Of Iteration Length - σ`,
+       std(datediff(iteration.endDate, iteration.startDate)) * std(datediff(iteration.endDate, iteration.startDate)) as `Deviation Of Iteration Length - σ^2`,
+       if(std(datediff(iteration.endDate, iteration.startDate)) > @maxSprintVariance, 1, 0) as `Anti-pattern occurrence`
+    from iteration
+    where iteration.superProjectId = @projectId
+            and
+          iteration.id != if(@excludeFirstAndLastIteration = true, @idOfFirstIteration, -1)
+            and
+          iteration.id != if(@excludeFirstAndLastIteration = true, @idOfLastIteration, -1)
+    order by iteration.startDate;
 
