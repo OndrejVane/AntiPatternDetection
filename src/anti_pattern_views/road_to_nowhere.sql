1556d1c38fce2f679abeaf109d15f5ec05832ed9
Ondřej Váně <onry.vane@gmail.com>
2021-02-16 19:42:37 +0100

    Road to nowhere
    * refactor code
    + add some comment

--- src/anti_pattern_views/road_to_nowhere.sql
+++ src/anti_pattern_views/road_to_nowhere.sql
@@ -8,41 +8,19 @@ Description: The project is not sufficiently planned and therefore
 Detection: There is no activity in ALM that would indicate the creation
            of a project plan. There will be no document in the wiki
            called the "Project Plan". Project plan should be created in first or
-           second iteration.
+           second iteration. Also could be detected with field change view. If is
+           a lot of changes on issues in the beginning of the iteration so then could
+           indicate some planning.
 
            TODO: 1) detekovat, že na začátku iterací probíhá plánování
+                    => pomocí fieldChangeView
+                    => zde není uvedeno, kdy se změnilo pole asignee (je zde pouze logtime a přidání/úprava komentáře)
+                    => teoreticky by se většina aktivit měla vytvářet na začátku iterace(mohou být už vytvořeny během iterace) a následně by se dalo detekovat, jestli
+                    proběhlo na začátku iterace nějaké plánování
 
 */
+select * from workunitview inner join fieldchangeview on workunitview.id = fieldchangeview.itemId where `projectId` = 5 order by `startDate`
 
-create or replace view road_to_nowhere_view as
-    select project.id as `ID`,
-           project.name as `Project Name`,
-           count(*) as `Count of issues`,
-           (select count(*)
-            from workunitview
-            where project.id = workunitview.projectID
-                and
-                (   workunitview.name like '%plán%projektu%' or
-                    workunitview.name like '%project%plan%' or
-                    workunitview.name like '%plan%project%'or
-                    workunitview.name like '%proje%plán%')) as `Project plan activities`,
-            IF((select count(*)
-            from workunitview
-            where project.id = workunitview.projectID
-                and
-                (   workunitview.name like '%plán%projektu%' or
-                    workunitview.name like '%project%plan%' or
-                    workunitview.name like '%plan%project%'or
-                    workunitview.name like '%proje%plán%')) >= 1, false, true) as `Anti-pattern occurrence`
-    from project inner join workunitview on project.id = workunitview.projectId
-    group by project.id
-    order by project.id;
-
-select * from road_to_nowhere;
-
-select * from fieldchangeview;
-
-/* NOVÁ VERZE */
 set @projectId = 5;
 set @firstIterationStartDate = (select startDate from iteration where superProjectId = @projectId ORDER BY startDate LIMIT 1 offset 0);
 set @secondIterationStartDate = (select startDate from iteration where superProjectId = @projectId ORDER BY startDate LIMIT 1 offset 1);
@@ -60,5 +38,5 @@ set @numberOfWikipagesForProjectPlan = (SELECT count(*) from artifactview where 
                     artifactview.name like '%plan%project%' or
                     artifactview.name like '%proje%plán%'));
 
-select @projectId, @numberOfIssuesForProjectPlan, @numberOfWikipagesForProjectPlan;
+select @projectId, @numberOfIssuesForProjectPlan, @numberOfWikipagesForProjectPlan, if(@numberOfWikipagesForProjectPlan = 0 AND @numberOfIssuesForProjectPlan = 0, 1, 0) as `Anti-pattern occurrence`;
 
