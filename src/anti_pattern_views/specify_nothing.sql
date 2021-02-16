c6b9686bde15e6aaef1a55ddce9c2a7bab857d18
Ondřej Váně <onry.vane@gmail.com>
2021-02-16 20:07:47 +0100

    Specify nothing
    + init file

---
+++ src/anti_pattern_views/specify_nothing.sql
@@ -0,0 +1,14 @@
+/*
+Anti-pattern name: Specify nothing
+
+Description: The specification is not done intentionally. Programmers are
+             expected to work better without written specifications.
+
+Detection: No specification artifact. There is no issue that will have something
+           like "DSP, SPECIFICATIONS, ETC." in the title. Initially, meetings
+           with the customer should be more frequent to clarify the project framework.
+            No entry in the wiki with the project specification.
+
+           TODO:
+*/
+
